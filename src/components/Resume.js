5c015b703e1e3e985bf6b06dbce7edefc1e4fa06
kushvanth <kushvanth2917@gmail.com>
2025-04-30 13:42:59 +0530

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { FaCode, FaServer, FaShieldAlt, FaGraduationCap, FaCertificate } from 'react-icons/fa';
-import { SiPython, SiCplusplus, SiHtml5, SiCss3, SiLinux, SiDocker, SiGit } from 'react-icons/si';
-import { BsShieldCheck } from 'react-icons/bs';
 
 const Resume = () => {
   // You will need to add your actual resume file to the public folder
@@ -17,51 +14,68 @@ const Resume = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-6xl mx-auto">
           <div>
             <h3 className="text-xl uppercase font-semibold mb-6 text-gray-300 flex items-center">
-              <FaCode className="mr-2" /> TECHNICAL SKILLS
+              <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+              </svg>
+              TECHNICAL SKILLS
             </h3>
             
             <div className="space-y-6">
               <div>
                 <h4 className="text-lg mb-2 flex items-center">
-                  <FaCode className="mr-2" /> Languages
+                  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+                  </svg>
+                  Languages
                 </h4>
                 <div className="flex items-center space-x-4 text-gray-400">
-                  <SiCplusplus className="text-2xl" />
-                  <SiPython className="text-2xl" />
+                  <span className="text-2xl">C++</span>
+                  <span className="text-2xl">Python</span>
                 </div>
               </div>
               
               <div>
                 <h4 className="text-lg mb-2 flex items-center">
-                  <FaServer className="mr-2" /> Web Technologies
+                  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2m-2-4h.01M17 16h.01" />
+                  </svg>
+                  Web Technologies
                 </h4>
                 <div className="flex items-center space-x-4 text-gray-400">
-                  <SiHtml5 className="text-2xl" />
-                  <SiCss3 className="text-2xl" />
+                  <span className="text-2xl">HTML</span>
+                  <span className="text-2xl">CSS</span>
                 </div>
               </div>
               
               <div>
                 <h4 className="text-lg mb-2 flex items-center">
-                  <FaShieldAlt className="mr-2" /> Professional Skills
+                  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
+                  </svg>
+                  Professional Skills
                 </h4>
                 <div className="flex flex-wrap items-center space-x-4 text-gray-400">
-                  <SiLinux className="text-2xl" />
-                  <SiGit className="text-2xl" />
-                  <SiDocker className="text-2xl" />
-                  <BsShieldCheck className="text-2xl" />
+                  <span className="text-2xl">Linux</span>
+                  <span className="text-2xl">Git</span>
+                  <span className="text-2xl">Docker</span>
+                  <span className="text-2xl">Security</span>
                 </div>
                 <p className="text-gray-400 mt-2">Network Security, Computer Forensics, Malware Analysis, Threat Management, IAM</p>
               </div>
             </div>
             
             <h3 className="text-xl uppercase font-semibold mb-6 mt-12 text-gray-300 flex items-center">
-              <FaCertificate className="mr-2" /> CERTIFICATIONS
+              <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+              </svg>
+              CERTIFICATIONS
             </h3>
             
             <div className="space-y-4">
               <div className="flex items-start">
-                <FaCertificate className="text-gray-400 mt-1 mr-3" />
+                <svg className="w-5 h-5 text-gray-400 mt-1 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+                </svg>
                 <div>
                   <h4 className="text-lg">Quick Heal Certified Digital Forensic Investigator</h4>
                   <p className="text-gray-400">January 2025</p>
@@ -69,7 +83,9 @@ const Resume = () => {
               </div>
               
               <div className="flex items-start">
-                <FaCertificate className="text-gray-400 mt-1 mr-3" />
+                <svg className="w-5 h-5 text-gray-400 mt-1 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+                </svg>
                 <div>
                   <h4 className="text-lg">CompTIA Security+</h4>
                   <p className="text-gray-400">January 2025</p>
@@ -77,7 +93,9 @@ const Resume = () => {
               </div>
               
               <div className="flex items-start">
-                <FaCertificate className="text-gray-400 mt-1 mr-3" />
+                <svg className="w-5 h-5 text-gray-400 mt-1 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+                </svg>
                 <div>
                   <h4 className="text-lg">CompTIA Network+</h4>
                   <p className="text-gray-400">July 2024</p>
@@ -85,7 +103,9 @@ const Resume = () => {
               </div>
               
               <div className="flex items-start">
-                <FaCertificate className="text-gray-400 mt-1 mr-3" />
+                <svg className="w-5 h-5 text-gray-400 mt-1 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+                </svg>
                 <div>
                   <h4 className="text-lg">Safeguarding Digital Foot Prints - Quick Heal</h4>
                   <p className="text-gray-400">August 2023</p>
